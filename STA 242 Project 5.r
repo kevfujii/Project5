3dd03ada1608fda1101c4238ebe49cbfd2115d3d
unknown <Erin K Melcon@.(none)>
2011-02-26 11:01:04 -0800

    Deleted a useless comment

--- STA 242 Project 5.r
+++ STA 242 Project 5.r
@@ -1,10 +1,3 @@
-List = list.files("enron",recursive = TRUE,full.names=TRUE)
-Emails = vector("list",length(List))
-lapply(1:length(List), function(i){ Emails[[i]]=readLines(List[i])})
-
-Test =list.files("enron/maildir/allen-p/",full.names=TRUE,recursive = TRUE)
-Testlong =lapply(1:length(Test), function(i){ Emails[[i]]=readLines(Test[i])})
-
 PeopleNames =list.files("enron/maildir/",full.names=TRUE)
 Email= lapply(1:length(PeopleNames), function(i){
   ByPerson =list.files(PeopleNames[i],full.names = TRUE,recursive = TRUE)
@@ -12,7 +5,3 @@ Email= lapply(1:length(PeopleNames), function(i){
       readLines(ByPerson[j])
     })
   })
-
-
-
-x= lapply(1:10,function(i){i})
